17d83690c564ca31fe2ffeb5d15ae38aaefb60ff
yunabin <yhb1020525@gmail.com>
2026-03-20 02:33:54 +0900

    feat: 투두 분석 기능 추가 (전체, 완료, 미완료 개수)

--- src/components/List.jsx
+++ src/components/List.jsx
@@ -18,9 +18,27 @@ const List = ({ todos, onUpdate, onDelete }) => {
 
   const filteredTodos = getFilteredData();
 
+  const getAnalyzedData = () => {
+    const totalCount = todos.length;
+    const doneCount = todos.filter((todo) => todo.isDone).length;
+    const notDoneCount = totalCount - doneCount;
+    return {
+      totalCount,
+      doneCount,
+      notDoneCount,
+    };
+  };
+
+  const { totalCount, doneCount, notDoneCount } = getAnalyzedData();
+
   return (
     <div className="List">
       <h4>Todo List 💪</h4>
+      <div>
+        <div>total: {totalCount}</div>
+        <div>done: {doneCount}</div>
+        <div>notDone: {notDoneCount}</div>
+      </div>
       <input
         value={search}
         onChange={onChangeSearch}
