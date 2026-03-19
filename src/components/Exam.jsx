a6895b083187c00cbfa494185f82aa656bc1d6d5
yunabin <yhb1020525@gmail.com>
2026-03-20 02:13:26 +0900

    refactor: useState 기반 로직을 useReducer로 전환

---
+++ src/components/Exam.jsx
@@ -0,0 +1,37 @@
+import { useReducer } from "react";
+
+function reducer(state, action) {
+  switch (action.type) {
+    case "INCREASE":
+      return state + action.data;
+    case "DECREASE":
+      return state - action.data;
+    default:
+      return state;
+  }
+}
+
+const Exam = () => {
+  const [state, dispatch] = useReducer(reducer, 0);
+  const onClickPlus = () => {
+    dispatch({
+      type: "INCREASE",
+      data: 1,
+    });
+  };
+  const onClickMinus = () => {
+    dispatch({
+      type: "DECREASE",
+      data: 1,
+    });
+  };
+
+  return (
+    <div>
+      <h1>{state}</h1>
+      <button onClick={onClickPlus}>+</button>
+      <button onClick={onClickMinus}>-</button>
+    </div>
+  );
+};
+export default Exam;
